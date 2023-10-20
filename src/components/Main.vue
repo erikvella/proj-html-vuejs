5a972523f8fceaf141534419d2e8d6dc2cccbe37
erikvella <vella.erik1@gmail.com>
2023-10-20 20:54:10 +0100

    [init] section4

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,11 +2,13 @@
 import Section1 from './partials/Section1.vue';
 import Section2 from './partials/Section2.vue';
 import Section3 from './partials/Section3.vue';
+import Section4 from './partials/Section4.vue';
 export default {
   components:{
    Section1,
    Section2,
-   Section3
+   Section3,
+   Section4
   },
 name:'Main'
 }
@@ -16,6 +18,7 @@ name:'Main'
   <Section1 />
   <Section2 />
   <Section3 />
+  <Section4 />
 </template>
 
 
---
+++ src/components/partials/Section4.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+name:'Section4'
+}
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      
+      <h1 class="text-center">LATEST ARTICLES</h1>
+      <div class="yellow-line my-5 mx-auto"></div>
+      <h4 class="dark-grey-text text-center">Together we make all the differences</h4>
+  </div>  
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-6"></div>
+      <div class="col-6"></div>
+    </div>
+  </div>
+  </section>
+</template>
+
+
+
+<style lang="scss" scoped>
+section{
+  height: 1000px;
+  width: 100%;
+}
+h1{
+  padding-top: 150px;
+}
+</style>
